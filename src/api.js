ed60b932ecf252ec6441a9f85e43965d9afea659
Nitha <alive1704@gmail.com>
2020-10-20 22:05:45 +0700

    update handle post 2

--- src/api.js
+++ src/api.js
@@ -1,32 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 
-const cors = require('cors');
-const serverless = require('serverless-http');
+const cors = require("cors");
+const serverless = require("serverless-http");
 
 app.use(cors());
-router.get('/',(req,res)=>{
 
-    const users = [
-      { id: 1, name: "puxer", lastname: "maxson" },
-      { id: 2, name: "Mary", lastname: "Swan" },
-      { id: 3, name: "John", lastname: "Doe" },
-    ]
+const users = [
+  { id: 1, name: "puxer", lastname: "maxson" },
+  { id: 2, name: "Mary", lastname: "Swan" },
+  { id: 3, name: "John", lastname: "Doe" },
+];
 
-    res.json(
-      users
-    );
-})
-
-router.post("/", (req, res) => {
-  res.send("successful");
+router.get("/", (req, res) => {
+  res.json(users);
 });
 
-router.get('/hell',(req,res)=>{
-    res.send('hell')
-})
+router.post("/", (req, res) => {
+  const user = {
+    id: req.body.id,
+    name: req.body.name,
+    lastname: req.body.lastname,
+  };
+  users.push(user);
+  res.send(req.body);
+});
 
-app.use('/.netlify/functions/api', router)
+router.get("/hell", (req, res) => {
+  res.send("hell");
+});
+
+app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
